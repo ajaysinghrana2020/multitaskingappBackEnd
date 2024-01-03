4b67d9dc71571c9746c97dab19712ee8407479a6
ajay <ajaysinghrana8894@gmail.com>
2024-01-03 12:39:13 +0530

    my first commit

---
+++ src/main/java/com/example/multitaskingappBackEnd/MultitaskingappBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.multitaskingappBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitaskingappBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitaskingappBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multitaskingappBackEnd/MultitaskingappBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multitaskingappBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitaskingappBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
