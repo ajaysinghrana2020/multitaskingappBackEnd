8b2a5c01bf39d01387c60bbbff1064f3e2d73200
ajay <ajaysinghrana8894@gmail.com>
2024-01-13 11:40:54 +0530

    using lombook and added title

--- src/main/java/com/example/multitaskingappBackEnd/Notes.java
+++ src/main/java/com/example/multitaskingappBackEnd/Notes.java
@@ -1,7 +1,15 @@
 package com.example.multitaskingappBackEnd;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Table(name = "notes")
 @Entity
 public class Notes {
@@ -9,61 +17,13 @@ public class Notes {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int notes_id;
     int time;
+    String Title;
+
     String note;
     Boolean status;
-    
+
     @ManyToOne
     private Users users;
-
-
-    public int getNotes_id() {
-        return notes_id;
-    }
-
-    public void setNotes_id(int notes_id) {
-        this.notes_id = notes_id;
-    }
-
-    public int getTime() {
-        return time;
-    }
-
-    public void setTime(int time) {
-        this.time = time;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public Boolean getStatus() {
-        return status;
-    }
-
-    public void setStatus(Boolean status) {
-        this.status = status;
-    }
-
-    public Notes() {
-
-    }
-
-    public Notes(int notes_id, int time, String note, Boolean status) {
-        this.notes_id = notes_id;
-        this.time = time;
-        this.note = note;
-        this.status = status;
-    }
-
-    public Users getUsers() {
-        return users;
-    }
-
-    public void setUsers(Users users) {
-        this.users = users;
-    }
 }
+
+
